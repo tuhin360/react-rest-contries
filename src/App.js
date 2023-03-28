059ade811d4b604a0b23cc6861f2d1aa8f5e6aa2
tuhin <tuhin153695@gmail.com>
2023-03-28 12:01:45 +0600

    rest contries done

---
+++ src/App.js
@@ -0,0 +1,57 @@
+
+import './App.css';
+import Countries from './components/Countries/Countries';
+ 
+function App() {
+  return (
+    <div className="App">
+      <Countries></Countries>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+ 
+
+
+// function LoadCountries(){
+//   const [countries, setCountries] = useState([]);
+
+//   useEffect( ()=>{
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then(res=>res.json())
+//     .then(data=>setCountries(data))
+//   }, [])
+  
+//   return(
+//     <div>
+//       <h2>Visiting every country of the world!!!!!</h2>
+//       <h3>Available Countries: {countries.length}</h3>
+//       {
+//         countries.map(country => <Country name={country.name.common}population={country.population}></Country>)
+//       }
+//     </div>
+//   )
+// }
+
+
+// function Country(props){
+//   return(
+//     <div>
+//        <h2>Name: {props.name}</h2>
+//        <h4>Population: {props.population}</h4>
+//     </div>
+//   )
+// }
+
+
+
+export default App;
+ 
